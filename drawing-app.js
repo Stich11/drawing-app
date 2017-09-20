894a321e1ea0aad8b660e8f49ab7dd84eb61db47
Stich11 <kid@donald.local>
2017-09-20 16:59:02 -0400

    compleat with tweaks

--- drawing-app.js
+++ drawing-app.js
@@ -1,89 +1,83 @@
-$(document).ready(function(){    
+$(document).ready(function(){ 
+    var width = $('.canvas1').width()
+    var height = $('.canvas1').height()
+    $('.canvas1')[0].height = height
+    $('.canvas1')[0].width = width
     var run = false;
     var hexcolor = "blue";
     var worh = false;
     var brush = "circle";
     var counter = 0;
     var slider = document.getElementById("myRange");
-    $(".dive").mouseup(function(){
+    $(".canvas1").mouseup(function(){
         run = false;
     });
-    $(".dive").mousedown(function(){
+    $(".canvas1").mousedown(function(){
         run = true;
     });
     $(document).mousemove(function(event){
         if(run){
             var left = event.pageX - (slider.value / 2);
             var top = event.pageY - (slider.value / 2);
-            if(left <= $('.dive').width() - slider.value){ // edit
-                if(left >= -2.5){
-                    if(top <= $('.dive').height() - slider.value){ // edit
-                        if(top >= -2.5){
-                            var $div = $('<div class="'+brush+'-base" style="height:'+slider.value+'px; width:'+slider.value+'px; background-color:'+hexcolor+';position:absolute;left:'+left+'px; top:'+top+'px;"></div>');
-                            $('.dive').append($div);
-                            $("span").text(event.pageX + ", " + event.pageY);
+            if(event.pageX < width){
+                if(event.pageX > 0){
+                    if(event.pageY < height){
+                        if(event.pageY > 0){
+                            if(brush === "circle"){
+                                var c = document.getElementById("myCanvas");
+                                var ctx = c.getContext("2d");
+                                ctx.beginPath();
+                                ctx.fillStyle = hexcolor
+                                ctx.arc(event.pageX, event.pageY, slider.value, 0, 2 * Math.PI);
+                                ctx.fill();
+                            }else{
+                                var c = document.getElementById("myCanvas");
+                                var ctx = c.getContext("2d");
+                                ctx.beginPath();
+                                ctx.rect(left, top, slider.value * 2, slider.value * 2);
+                                ctx.fillStyle = hexcolor;
+                                ctx.fill();
+                            }   
                         }else{
                             run = false
-                        };
+                        } 
                     }else{
                         run = false
-                    };
+                    } 
                 }else{
                     run = false
-                }; 
+                } 
             }else{
                 run = false
-            };  
+            }     
         };
     });
-    setInterval(function(){
-        if($('.inputingcolor')[0].value !== hexcolor){
-            if(worh){
-                hexcolor = $('.inputingcolor')[0].value
-            }
-        }
-    },100)
-    $('.menu2').click(function(){
-        worh = true
-    })
-    $('.menu1').click(function(){
-        worh = false
-    })
-    $('.home').click(function(){
-        worh = false
-    })
+    
     $('.addColor').click(function(){
-        if(worh){
-            var alredy = false
-            var arr = $('.plate')
-            arr = arr[0].children
-            var color = $('.inputingcolor')[0].value
-            for(let s = 1; s < arr.length;s++){
-                if(!alredy){
-                    alredy = arr[s].style.background === color
-                } 
-            }
-            console.log(alredy)
+        var alredy = false
+        var arr = $('.plate')
+        arr = arr[0].children
+        var color = $('.inputingcolor')[0].value
+        for(let s = 3; s < arr.length;s++){
             if(!alredy){
-                counter++
-                if(color === "#ffffff"){
-                    var $divee = $('<div class="colorp hi'+counter+'" style="border:1px solid black;background-color:'+color+';"></div>')
-                }else{
-                    var $divee = $('<div class="colorp hi'+counter+'" style="background-color:'+color+';"></div>')
-                }
-                $('.plate').append($divee)
-                $('.hi'+counter+'').click(function(){
-                    if(!worh){
-                        hexcolor = event.toElement.style.backgroundColor
-                    } 
-                })
+                alredy = arr[s].style.background == color
             } 
         }
+        if(!alredy){
+            counter++
+            if(color === "#ffffff"){
+                var $divee = $('<div class="colorp hi'+counter+'" style="border:1px solid black;background-color:'+color+';"></div>')
+            }else{
+                var $divee = $('<div class="colorp hi'+counter+'" style="background-color:'+color+';"></div>')
+            }
+            $('.plate').append($divee)
+            $('.hi'+counter+'').click(function(){
+                hexcolor = event.toElement.style.backgroundColor
+            })
+        } 
     })
     $('.colorp').click(function(){
-        if(!worh){
-            hexcolor = event.toElement.style.backgroundColor
-        } 
+        hexcolor = event.toElement.style.backgroundColor
     })
     $('.circle').click(function(){
         brush = "circle"
@@ -96,5 +90,4 @@ $(document).ready(function(){
       var $mydive = $('<div class="deme '+brush+'-base" style="border:1px solid black; width:'+slider.value+'px; height:'+slider.value+'px; "></div>');
       $('.demo').append($mydive)
     }
-
 });
