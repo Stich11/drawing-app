27c3bdd20115cc32060f42453c3436ceb76d9049
Stich11 <kid@donald.local>
2017-09-20 09:03:53 -0400

    tweks

--- drawing-app.js
+++ drawing-app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var hexcolor = "blue";
     var worh = false;
     var brush = "circle";
+    var counter = 0;
     var slider = document.getElementById("myRange");
     $(".dive").mouseup(function(){
         run = false;
@@ -39,27 +40,61 @@ $(document).ready(function(){
         if($('.inputingcolor')[0].value !== hexcolor){
             if(worh){
                 hexcolor = $('.inputingcolor')[0].value
-                console.log(hexcolor)
             }
         }
     },100)
-    $('.toggleinput').on("click", function(){
-        worh = event.toElement.checked
+    $('.menu2').click(function(){
+        worh = true
     })
-    $('.colorp').on("click",function(){
+    $('.menu1').click(function(){
+        worh = false
+    })
+    $('.home').click(function(){
+        worh = false
+    })
+    $('.addColor').click(function(){
+        if(worh){
+            var alredy = false
+            var arr = $('.plate')
+            arr = arr[0].children
+            var color = $('.inputingcolor')[0].value
+            for(let s = 1; s < arr.length;s++){
+                if(!alredy){
+                    alredy = arr[s].style.background === color
+                } 
+            }
+            console.log(alredy)
+            if(!alredy){
+                counter++
+                if(color === "#ffffff"){
+                    var $divee = $('<div class="colorp hi'+counter+'" style="border:1px solid black;background-color:'+color+';"></div>')
+                }else{
+                    var $divee = $('<div class="colorp hi'+counter+'" style="background-color:'+color+';"></div>')
+                }
+                $('.plate').append($divee)
+                $('.hi'+counter+'').click(function(){
+                    if(!worh){
+                        hexcolor = event.toElement.style.backgroundColor
+                    } 
+                })
+            } 
+        }
+    })
+    $('.colorp').click(function(){
         if(!worh){
-            hexcolor = event.toElement.classList[1]
+            hexcolor = event.toElement.style.backgroundColor
         } 
     })
-    $('.circle').on("click",function(){
+    $('.circle').click(function(){
         brush = "circle"
     })
-    $('.square').on("click",function(){
+    $('.square').click(function(){
         brush = "square"
     })
     slider.oninput = function() {
     $('.deme').remove()
       var $mydive = $('<div class="deme '+brush+'-base" style="border:1px solid black; width:'+slider.value+'px; height:'+slider.value+'px; "></div>');
       $('.demo').append($mydive)
-}
+    }
+
 });
